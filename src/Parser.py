83105c4c0c6dc5128796fa9ab37f7016b0bdc8e7
sol455 <sol@glowinthedark.co.uk>
2022-01-29 16:43:21 +0000

    Added http request to retrieve JSON

--- src/Parser.py
+++ src/Parser.py
@@ -1,3 +1,20 @@
 #! Python3
 
+import requests
+import json
 
+def get_json():
+    r = requests.get('https://api.ampifymusic.com/packs')
+    return r.json()
+
+
+def sort(text):
+    print(json.dumps(text, indent=4, sort_keys=True))
+
+
+def run():
+    data = get_json()
+    sort(data)
+
+
+run()
