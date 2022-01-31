6a5e9a7f0cfe7b1d07ae73fb10d2f85412760986
sol455 <sol@glowinthedark.co.uk>
2022-01-31 11:45:33 +0000

    Updated tests for sorted data

--- src/test_parser.py
+++ src/test_parser.py
@@ -12,12 +12,6 @@ class ParseTests(unittest.TestCase):
 
     def test_fetch_data(self):
         fetched_data = get_json()
-
-        # file_path = os.path.join(DATA_DIR, "data.json")
-        # print (file_path)
-        # f = open(file_path, "w")
-        # f.write(json.dumps(fetched_data, indent=4))
-        # f.close()
         self.assertIsNotNone(fetched_data)
 
     def test_get_sorted_genres(self):
@@ -27,7 +21,7 @@ class ParseTests(unittest.TestCase):
             data = json.loads(f.read())
 
         genre_names = get_sorted_genres(data)
-        self.assertTrue(len(genre_names) > 0)
+        self.assertTrue(len(genre_names) == 47)
 
         for i in range(len(genre_names) - 2):
             self.assertTrue(genre_names[i] < genre_names[i + 1])
@@ -39,7 +33,10 @@ class ParseTests(unittest.TestCase):
             data = json.loads(f.read())
 
         hiphop_packs = get_hiphop_packs(data)
-        self.assertTrue(len(hiphop_packs) > 0)
+        self.assertTrue(len(hiphop_packs) == 3)
 
         for pack in hiphop_packs:
             self.assertTrue(HIP_HOP in pack["genres"])
+
+
+
